6bc16b6acb8ee0832f908765bbc5fa162b53c44e
Kelsey Jackson <kelseythej@gmail.com>
2017-03-21 14:09:04 -0500

    more work

--- js/script.js
+++ js/script.js
@@ -1,13 +1,28 @@
 const log = (statement) => console.log(statement),
+      paymentForm = document.getElementsByTagName('FORM')[0],
       nameInput = document.getElementById('name'),
+      emailInput = document.getElementById('mail'),
       titleSelection = document.getElementById('title'),
       colorSelect = document.getElementById('color'),
       designSelect = document.getElementById('design'),
-      colors = colorSelect.children;
+      colors = colorSelect.children,
+      paymentSection = document.getElementsByTagName('FIELDSET')[3],
+      ccNumberInput = document.getElementById('cc-num'),
+      ccZip = document.getElementById('zip'),
+      cvv = document.getElementById('cvv'),
+      noJsLabel = document.querySelector('label[for=other-title]'),
+      noJsInput = document.querySelector('input#other-title'),
+      creditCardDiv = document.getElementById('credit-card');
 
+
+
+noJsLabel.classList.add('hide');
+noJsInput.classList.add('hide');
 
 // Sets the focus to the first input when the page first loads
 nameInput.focus();
+
+
 
 
 // If 'Other' is selected for the job role, an 'other title' text fiels is revealed
@@ -17,11 +32,11 @@ titleSelection.addEventListener('change', ()=> {
         const otherJobRoleInput = document.createElement('input'),
               otherJobRoleLabel = document.createElement('label');
         otherJobRoleInput.type = 'text';
-        otherJobRoleInput.id = 'other-title';
-        otherJobRoleInput.name = 'other-title';
+        otherJobRoleInput.id = 'other-title-js';
+        otherJobRoleInput.name = 'other-title-js';
         otherJobRoleInput.placeholder = 'Your Job Role';
         otherJobRoleLabel.innerText = 'Your Job Role:';
-        otherJobRoleLabel.htmlFor = 'other-title'; 
+        otherJobRoleLabel.htmlFor = 'other-title-js';
         firstFieldset.appendChild(otherJobRoleLabel);
         firstFieldset.appendChild(otherJobRoleInput);
     }
@@ -41,7 +56,7 @@ colorOption.selected = true;
 // log(colorSelect.firstElementChild);
 colorSelect.style.visibility = 'hidden';
 colorSelect.previousElementSibling.style.visibility = 'hidden';
-colorSelect.insertBefore(colorOption, colorSelect.firstElementChild)
+colorSelect.insertBefore(colorOption, colorSelect.firstElementChild);
 
 designSelect.addEventListener('change', ()=> {
     let dynamicSelect = '';
@@ -107,3 +122,68 @@ activitySection.addEventListener('change', (e)=> {
         }
     }
 });
+
+paymentSection.lastElementChild.classList.add('hide');
+paymentSection.lastElementChild.previousElementSibling.classList.add('hide');
+paymentSection.addEventListener('change', (e)=> {
+   if(e.target.value === 'credit card') {
+       // log(e.target.value);
+       creditCardDiv.classList.remove('hide');
+       paymentSection.lastElementChild.classList.add('hide');
+       paymentSection.lastElementChild.previousElementSibling.classList.add('hide');
+   } else if (e.target.value === 'paypal') {
+       creditCardDiv.classList.add('hide');
+       paymentSection.lastElementChild.classList.add('hide');
+       paymentSection.lastElementChild.previousElementSibling.classList.remove('hide');
+   } else if (e.target.value === 'bitcoin') {
+       creditCardDiv.classList.add('hide');
+       paymentSection.lastElementChild.previousElementSibling.classList.add('hide');
+       paymentSection.lastElementChild.classList.remove('hide');
+   }
+});
+
+
+log(paymentSection);
+
+paymentForm.addEventListener('submit', (e)=>{
+    if(nameInput.value === '') {
+        e.preventDefault();
+        log('name can\'t be blank');
+        nameInput.classList.add('error');
+    }
+    if(!emailInput.value.includes('@') || !emailInput.value.includes('.')) {
+        e.preventDefault();
+        log('email is not properly formatted');
+        emailInput.classList.add('error');
+    }
+
+    if(conferencePrice === 0) {
+        e.preventDefault();
+        log('no activities selected');
+        activitySection.firstElementChild.classList.add('error-text');
+    }
+
+    if(ccNumberInput.value.length < 13) {
+        e.preventDefault();
+        log('number must not be less than 13 characters');
+        ccNumberInput.classList.add('error');
+    } else if (ccNumberInput.value.length > 16) {
+        e.preventDefault();
+        log('number must not be more than 16 characters');
+        ccNumberInput.classList.add('error');
+    }
+
+    if(ccZip.value.length !== 5) {
+        e.preventDefault();
+        log('number can only be 5 characters');
+        ccZip.classList.add('error');
+    }
+
+    if(cvv.value.length !== 3) {
+        e.preventDefault();
+        log('cvv can only be 3 numbers');
+        cvv.classList.add('error');
+    }
+
+});
+
